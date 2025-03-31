26ffe1d9416d60509ca3e933a798de411c09ce29
AnnuArchon <minastyr@gmail.com>
2025-03-31 18:15:22 -0400

    after seed cleanup

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,3 +1,17 @@
 import Question from './Question.js';
+import QuestionSchema from './Question.js';
+import mongoose from 'mongoose';
+import QuestionModel from './Question.js';
 
-export default { Question };
+
+//export default { Question };
+
+const models = {
+    Question: {
+      db: mongoose.connection, // Assuming you're using Mongoose for MongoDB
+      model: QuestionModel,    // The actual Mongoose model
+    },
+  };
+
+
+export default { Question, QuestionSchema, models };
